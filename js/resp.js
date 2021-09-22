5e9312677f88725219e9c00dbe68699a06ead8c7
Diwesh <prashardiwesh@gmail.com>
2021-09-22 17:40:57 +0530

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp')
+    nav-list.classList.toggle('v-class-resp')
+   navbar.classList.toggle('h-nav-resp')
+});
+
+
